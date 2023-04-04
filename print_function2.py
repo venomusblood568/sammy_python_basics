1d64f9aed6accac355fb79b7db2f35eff9498c7f
GOURAV_ANAND_JHA <90522738+venomusblood568@users.noreply.github.com>
2023-04-05 01:32:03 +0530

    Create print_function2.py

---
+++ print_function2.py
@@ -0,0 +1,4 @@
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(1,n+1): #this is the range starting from 1 to n+1
+        print(i,end='') #end = '' helps you print all the number in same line rather than starting from new line reach..
